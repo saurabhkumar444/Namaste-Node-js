b15df814d116a5b1698cd6ef5588bf95787f33b9
Saurabh Maurya <saurabh.maurya@quantique.ai>
2024-08-25 20:36:17 +0530

    change commonjs to mudule exporting version

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-require("./secondCode");
-const { calculate, sumFun } = require("./sum");
+// require("./secondCode");
+
+// commonjs version
+// const { calculate, sumFun } = require("./sum");
+
+// module version
+import { calculate, sumFun } from "./sum.js";
+
 // const calculate = require("./sum");
 const firstName = "saurabh maurya";
 console.log("first name: " + firstName);
@@ -11,4 +17,4 @@ const sumData = calculate(10, 30);
 const sumFunData = sumFun;
 console.log(sumData, sumFunData);
 // console.log(globalThis === this);
-console.log(globalThis === global);
+// console.log(globalThis === global);
--- sum.js
+++ sum.js
@@ -1,9 +1,17 @@
 // modules protected thrir variables and functions from leaking, you can not access outside of other modules
 
-const sumFun = 30;
-function calculate(a, b) {
+// common js
+// const sumFun = 30;
+// function calculate(a, b) {
+//   const sum = a + b;
+//   return sum;
+// }
+// // here we exports
+// module.exports = { calculate, sumFun };
+
+export const sumFun = 30;
+export function calculate(a, b) {
   const sum = a + b;
   return sum;
 }
 // here we exports
-module.exports = { calculate, sumFun   };
