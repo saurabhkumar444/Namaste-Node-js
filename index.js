114b3e34f299af80f1b01b3c538cb32aec1f93df
Saurabh Maurya <saurabh.maurya@quantique.ai>
2024-08-25 19:54:17 +0530

    learn global objects

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const firstName = "saurabh maurya";
-console.log("first name: " + firstName);
+// console.log("first name: " + firstName);
+
+console.log(global);
+console.log(this);
+console.log(globalThis);
+
+console.log(globalThis === this);
+console.log(globalThis === global);
