664c518687e0a47cd0080f74d1866ad76809428c
Saurabh Maurya <saurabh.maurya@quantique.ai>
2024-11-15 20:44:08 +0530

    thread pool

--- threadpool.js
+++ threadpool.js
@@ -1,12 +1,59 @@
+process.env.UV_THREADPOOL_SIZE = 5;
 const crypto = require("crypto");
 
-function runTask(taskNumber) {
-  console.log(`Task ${taskNumber} started`);
-  crypto.pbkdf2("password", "salt", 100, 512, "sha512", () => {
-    console.log(`Task ${taskNumber} completed`);
-  });
-}
+// depands on thread and availability
 
-for (let i = 1; i <= 6; i++) {
-  runTask(i);
-}
+// function runTask(taskNumber) {
+//   console.log(`Task ${taskNumber} started`);
+//   crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+//     console.log(`Task ${taskNumber} completed`);
+//   });
+// }
+
+// sync operation
+// function runTask(taskNumber) {
+//   console.log(`Task ${taskNumber} started`);
+//   const data = crypto.pbkdf2Sync("password", "salt", 100, 50, "sha512");
+//   console.log(`Task ${taskNumber} completed`);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   runTask(i);
+// }
+
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+  console.log(`Task 1 completed`);
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+  console.log(`Task 2 completed`);
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+  console.log(`Task 3 completed`);
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+  console.log(`Task 4 completed`);
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+  console.log(`Task 5 completed`);
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+  console.log(`Task 6 completed`);
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+  console.log(`Task 7 completed`);
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+  console.log(`Task 8 completed`);
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+  console.log(`Task 9 completed`);
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+  console.log(`Task 10 completed`);
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+  console.log(`Task 11 completed`);
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", () => {
+  console.log(`Task 12 completed`);
+});
